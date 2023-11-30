9ca2ec4881ccd26c43cb11fae2edde8b7c6563bc
rlaalsrl715 <minkik715@gmail.com>
2023-11-30 21:19:18 +0900

    init

---
+++ src/main/kotlin/com/study/aop/AopStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.study.aop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AopStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<AopStudyApplication>(*args)
+}
